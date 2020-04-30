7d9bd932dff48b9808cdac574ee345f4fe8b1346
Parker <starrpe@gmail.com>
2020-04-29 23:47:43 -0400

    NM-302: Code restructuring and filters modal
    Filters modal created. Able to be opened and closed from the main map view. In-progress, will add checkbox options that can be toggled for demonstrative purposes without functionality.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@
  * NinerMaps
  * Main body of the NinerMaps app set-up
  */
-
 import React from 'react';
-import { StyleSheet, Text, View, Image, Alert, Button, TouchableOpacity } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Alert,
+    Button,
+    TouchableOpacity,
+    TouchableHighlight,
+    TextInput,
+    Modal
+} from 'react-native';
+import MapView, {
+    PROVIDER_GOOGLE,
+    Marker,
+    Callout
+} from 'react-native-maps';
 
 /**
  * Local imports
@@ -13,8 +27,6 @@ import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
 import { housing } from './resources/markers/housing'
 import { academic } from './resources/markers/academic'
 import { mapstyle } from './resources/MapStyle'
-
-
 
 /**
 * Region: initial position of the map is on UNCC lat/long
@@ -26,19 +38,18 @@ import { mapstyle } from './resources/MapStyle'
 export default class App extends React.Component {
 
     showMessage = () => {
-
         Alert.alert (
-
-            'hi chase',
-
-            
-
+            'test',
         )
     }
-   
 
-
-
+    state = {
+        isVisible: false
+      };
+    
+      displayFiltersModal(show){
+        this.setState({isVisible: show})
+      }
 
     render() {
         return (
@@ -55,49 +66,18 @@ export default class App extends React.Component {
                     longitudeDelta: 0.01
                     }}>
                           
-
                       <Marker 
                           coordinate={{latitude: 35.308802, longitude:  -80.733709}}
                           markerImage={'..\\assets\\pins\\dorms.png'}
                           pinColor={'green'}>
-                              <Callout >
-                                  
-                              <Text style={styles.callOutHeading}>
-                                    Student Union {"\n"}
-                                </Text>
-                                
-                                <TouchableOpacity
-                               
-                                 onPress={this.showMessage}
-                                 >
-                                
-                                <Text  style={styles.buttons}>Dining and Services</Text>
-        
-                                </TouchableOpacity>
-                                   
-
-                                    
-                                    
-                                         
-                                    
-                                         
-                                          <Text style={styles.buttons}>
-                                                          Building Contacts
-                                      
-                                      
-                                      </Text> 
-                              
-
+                              <Callout> 
+                              <Text style={styles.callOutHeading}> Student Union {"\n"} </Text>
+                                <TouchableOpacity onPress={this.showMessage}>
+                                    <Text style={styles.buttons}>Dining and Services</Text>
+                                </TouchableOpacity>  
+                                <Text style={styles.buttons}> Building Contacts </Text> 
                               </Callout>
-                      
-                    
-                        
-                                
                         </Marker>
-
-
-
-
 
                 {
                     housing.map(marker =>(
@@ -108,29 +88,16 @@ export default class App extends React.Component {
                                 title = {marker.title}
                                 key = {marker.tag}
                                 pinColor = {'gold'}>
-                                
                                 <Callout>
-                                <Text style={styles.callOutHeading}>
-                                {marker.title}
-                                </Text>
-
-                                <Text style={styles.buttons}>
-                                                          Building Contacts
-                                      
-                                      
-                                      </Text>
-
+                                    <Text style={styles.callOutHeading}>{marker.title}</Text>
+                                    <Text style={styles.buttons}> Building Contacts </Text>
                                 </Callout>
-
-
-
                         </Marker>
                     ))
                 }
                 {
                     academic.map(marker =>(
-                        <Marker 
-                        
+                        <Marker  
                             coordinate = {{
                                 latitude: marker.latitude,
                                 longitude: marker.longitude}}
@@ -138,41 +105,47 @@ export default class App extends React.Component {
                                 key = {marker.tag}
                                 pinColor = {'red'}
                                 calloutImage= {marker.calloutImage}>
-
-<Callout>
-
-<Text style={styles.callOutHeading}>
-                {marker.title}
-                </Text>
-                <Text style={styles.buttons}>
-                          Services
-      
-      
-      </Text>
-
-</Callout>
-
-
-
-
-
-
-                                
+                            <Callout>
+                                <Text style={styles.callOutHeading}>{marker.title}</Text>
+                                <Text style={styles.buttons}>Services</Text>
+                            </Callout>                      
                         </Marker>
                     ))
                 }
-
                 </MapView>
-            </View>
 
 
 
 
-                
+            <Modal
+            animationType = {"slide"}
+            transparent={false}
+            visible={this.state.isVisible}
+            >
+              <Text style = { styles.filtersModalText }>
+                  Filters options here</Text>
+              <Text 
+                style={styles.filtersModalCloseText}
+                onPress={() => {
+                  this.displayFiltersModal(!this.state.isVisible);}}>Close</Text>
+          </Modal>
+
+         <View style = { styles.modalContainer }>
+            <TouchableOpacity
+                style={styles.modalButton}
+                onPress={() => {
+                    this.displayFiltersModal(true);
+                }}>
+                <Text style={{color: 'white'}}>Filters</Text>
+            </TouchableOpacity>               
+        </View>
+
+
+    </View>
+
         );
      }
 }
-
 
 const styles = StyleSheet.create({
     container: {
@@ -212,9 +185,35 @@ const styles = StyleSheet.create({
                 textAlign: 'center',
                 fontWeight: 'bold', 
                 color:'#08701C'
+    },
 
-    }
-
-    
+    modalContainer: {
+        padding: 25,
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+      },
+      modalButton: {
+        borderColor: 'black',
+        paddingBottom: 10,
+        display: 'flex',
+        height: 30,
+        borderRadius: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 50,
+        backgroundColor: '#00703c',
+        shadowColor: '#2AC062',
+        shadowOpacity: 0.5,
+        shadowOffset: { 
+          height: 10, 
+          width: 0 
+        },
+        shadowRadius: 25,
+      },
+      filtersModalCloseText: {
+        fontSize: 24,
+        color: '#00703c',
+        textAlign: 'center'}
 
 });
